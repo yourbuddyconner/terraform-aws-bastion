e02650a854e087b5c2a2ec705172172727967d54
Conner Swann <me@connerswann.me>
2018-08-24 23:46:43 -0700

    added bastion host security group output

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,7 @@ output "private_instances_security_group" {
 output "bastion_host_role_id" {
   value = "${aws_iam_role.bastion_host_role.id}"
 }
+
+output "bastion_host_security_group_id" {
+  value = "${aws_security_group.bastion_host_security_group.id}"
+}
